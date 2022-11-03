45c41175d14df87e88207ca8050aed0ca3cae9dc
Francis Baldon <francisjohn.baldon@bisu.edu.ph>
2022-11-03 20:06:56 +0800

    Second attempt

--- .updater.sh
+++ .updater.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
 echo "Syncing..."
-read -p "Enter commit message:" message
+read -r -p "Enter commit message: " message
 
 cat ~/.bashrc > .bashrc
 cat ~/.config/nvim/init.vim > init.vim
 
 git add .
-git commit -m $message
+git commit -m "$message"
 git push
 
 echo "Repository updated!!"
