c3fba19877677559d824403a7639a59b2a319f26
Francis Baldon <francisjohn.baldon@tutanota.com>
2022-12-13 06:21:27 +0800

    Update

--- updater.sh
+++ updater.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cp -r ~/.config/nvim .config
-cat ~/.xinitrc > ~/projects/dotfiles/.xinitrc
-#cat ~/.zprofile > ~/projects/dotfiles/.zprofile
-#cat ~/.zshrc > ~/projects/dotfiles/.zshrc
+cat ~/.xinitrc > ~/desktop/dotfiles/.xinitrc
+#cat ~/.zprofile > ~/desktop/dotfiles/.zprofile
+#cat ~/.zshrc > ~/desktop/dotfiles/.zshrc
 
 git add --all
 read -p "Enter commit message: " message
