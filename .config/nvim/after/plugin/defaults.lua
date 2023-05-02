4efebf5c4cab6845996a7c3f2198fed95650d9bb
Francis John Baldon <francisjohnt.baldon@bisu.edu.ph>
2023-05-02 18:13:50 +0800

    testing keys

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -1,7 +1,6 @@
 vim.opt.relativenumber = true
+vim.opt.background = 'light'
+vim.cmd.highlight 'clear SignColumn'
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>p', ':Ex<CR>', { desc = '[p]netrw' })
-
-vim.cmd[[ syntax off ]]
-vim.cmd[[ highlight SignColumn ctermbg=NONE guibg=NONE ]]
+vim.keymap.set('n', '<leader>n', ':Ex<CR>', { desc = '[N]etrw' })
